3af8af7addf0499b0b929f7afaac829a153d9a2c
Patrick Titzler <ptitzler@us.ibm.com>
2018-01-29 07:52:51 -0800

    refresh to pick up PR 53

--- shoppinglist.js
+++ shoppinglist.js
@@ -2,7 +2,7 @@
 // this will be the PouchDB database
 var db = new PouchDB('shopping');
 
-// template shopping list
+// template shopping list object
 const sampleShoppingList = {
   "_id": "",
   "type": "list",
@@ -20,7 +20,7 @@ const sampleShoppingList = {
   "updatedAt": ""
 };
 
-// template shopping list item
+// template shopping list item object
 const sampleListItem = {
   "_id": "",
   "type": "item",
@@ -31,14 +31,27 @@ const sampleListItem = {
   "updatedAt": ""
 };
 
-// sort comparison function to sort an objected by "updateAt" field
+/**
+ * Sort comparison function to sort an object by "createdAt" field
+ *
+ * @param  {String} a
+ * @param  {String} b
+ * @returns {Number}
+ */
 const newestFirst = (a, b) => {
   if (a.createdAt > b.createdAt) return -1;
   if (a.createdAt < b.createdAt) return 1;
   return 0 
 };
 
-// perform AJAX request
+/**
+ * Perform an "AJAX" request i.e call the URL supplied with the 
+ * a querystring constructed from the supplied object
+ *
+ * @param  {String} url 
+ * @param  {Object} querystring 
+ * @returns {Promise}
+ */
 const ajax = function (url, querystring) {
   return new Promise(function(resolve, reject) {
 
@@ -46,7 +59,6 @@ const ajax = function (url, querystring) {
     var qs = [];
     for(var i in querystring) { qs.push(i + '=' + encodeURIComponent(querystring[i]))}
     url = url + '?' + qs.join('&');
-    console.log('GET', url);
 
     // make HTTP GET request
     var xmlhttp = new XMLHttpRequest();
@@ -76,7 +88,11 @@ Vue.material.registerTheme('default', {
   background: 'grey'
 });
 
-// this is the Vue.js app
+// this is the Vue.js app. It contains
+// el - the HTML element where the app is rendered
+// data - the data the app needs to be rendered
+// computed - derived data required for the display logic
+// method - JavaScript functions
 var app = new Vue({
   el: '#app',
   data: {
@@ -92,13 +108,16 @@ var app = new Vue({
     syncURL:'',
     syncStatus: 'notsyncing'
   },
+  // computed functions return data derived from the core data.
+  // if the core data changes, then this function will be called too.
   computed: {
-
-    // computed functions return data derived from the core data.
-    // if the core data changes, then this function will be called too.
+    /**
+     * Calculates the counts of items and which items are checked
+     * grouped by shopping list
+     * 
+     * @returns {Object}
+     */
     counts: function() {
-      // calculate the counts of items and which items are checked,
-      // grouped by shopping list
       var obj = {};
       // count #items and how many are checked
       for(var i in this.shoppingListItems) {
@@ -113,14 +132,28 @@ var app = new Vue({
       }
       return obj;
     },
+    /**
+     * Calculates the shopping list but sorted into
+     * date order - newest first
+     * 
+     * @returns {Array}
+     */
     sortedShoppingLists: function() {
       return this.shoppingLists.sort(newestFirst);
     },
+    /**
+     * Calculates the shopping list items but sorted into
+     * date order - newest first
+     * 
+     * @returns {Array}
+     */
     sortedShoppingListItems: function() {
       return this.shoppingListItems.sort(newestFirst);
     }
   },
-  // called once at app startup
+  /**
+   * Called once when the app is first loaded
+   */
   created: function() {
 
     // create database index on 'type'
@@ -146,21 +179,40 @@ var app = new Vue({
       };
       return db.find(q);
     }).then((data) => {
+      // write the shopping list items to the Vue model
       app.shoppingListItems = data.docs;
 
-      // load settings
+      // load settings (Cloudant sync URL)
       return db.get('_local/user');
     }).then((data) => {
-      // if we have settings
+      // if we have settings, start syncing
       this.syncURL = data.syncURL;
       this.startSync();
     }).catch((e) => {})
 
   },
   methods: {
+    /**
+     * Called when the settings button is pressed. Sets the mode
+     * to 'settings' so the Vue displays the settings panel.
+     */
     onClickSettings: function() {
       this.mode = 'settings';
     },
+    /**
+     * Called when the about button is pressed. Sets the mode
+     * to 'about' so the Vue displays the about panel.
+     */
+    onClickAbout: function() {
+      this.mode = 'about';
+    },    
+    /**
+     * Saves 'doc' to PouchDB. It first checks whether that doc
+     * exists in the database. If it does, it overwrites it - if
+     * it doesn't, it just writes it. 
+     * @param {Object} doc
+     * @returns {Promise}
+     */
     saveLocalDoc: function(doc) {
       return db.get(doc._id).then((data) => {
         doc._rev = data._rev;
@@ -169,6 +221,10 @@ var app = new Vue({
         return db.put(doc);
       });
     },
+    /**
+     * Called when save button on the settings panel is clicked. The
+     * Cloudant sync URL is saved in PouchDB and the sync process starts.
+     */
     onClickStartSync: function() {
       var obj = {
         '_id': '_local/user',
@@ -178,6 +234,12 @@ var app = new Vue({
         this.startSync();
       });
     },
+    /**
+     * Called when the sync process is to start. Initiates a PouchDB to
+     * to Cloudant two-way sync and listens to the changes coming in
+     * from the Cloudant feed. We need to monitor the incoming change
+     * so that the Vue.js model is kept in sync.
+     */
     startSync: function() {
       this.syncStatus = 'notsyncing';
       if (this.sync) {
@@ -218,7 +280,6 @@ var app = new Vue({
                 // remove it
                 arr.splice(match.i, 1);
               } else {
-                console.log('found a match', match);
                 // modify it
                 delete change._revisions;
                 Vue.set(arr, match.i, change);
@@ -242,8 +303,17 @@ var app = new Vue({
       });;
     },
 
-    // given a list of docs and an id, find the doc
-    // in the list that has an _id that matches the incoming id
+    /**
+     * Given a list of docs and an id, find the doc in the list that has
+     * an '_id' (key) that matches the incoming id. Returns an object 
+     * with the 
+     *   i - the index where the item was found
+     *   doc - the matching document
+     * @param {Array} docs
+     * @param {String} id
+     * @param {String} key
+     * @returns {Object}
+     */
     findDoc: function (docs, id, key) {
       if (!key) {
         key = '_id';
@@ -258,8 +328,16 @@ var app = new Vue({
       return { i: i, doc: doc };
     },
 
-    // find the id in docs and then 
-    // write it to PouchDB and keep the revision token
+    /**
+     * Given a list of docs and an id, find the doc in the list that has
+     * an '_id' (key) that matches the incoming id. Updates its "updatedAt"
+     * attribute and write it back to PouchDB.
+     *   i - the index where the item was found
+     *   doc - the matching document
+     * @param {Array} docs
+     * @param {String} id
+
+     */
     findUpdateDoc: function (docs, id) {
 
       // locate the doc
@@ -284,8 +362,11 @@ var app = new Vue({
       }
     },
 
-    // when the user has hit the big + buton to say they want to
-    // add a new shopping list
+    /**
+     * Called when the user clicks the Add Shopping List button. Sets
+     * the mode to 'addlist' to reveal the add shopping list form and
+     * resets the form variables.
+     */
     onClickAddShoppingList: function() {
 
       // open shopping list form
@@ -298,8 +379,11 @@ var app = new Vue({
       this.mode='addlist';
     },
 
-    // when we know the name of the new shopping list we can
-    // timestamp the object and save it
+    /**
+     * Called when the Save Shopping List button is pressed.
+     * Writes the new list to PouchDB and adds it to the Vue 
+     * model's shoppingLists array
+     */
     onClickSaveShoppingList: function() {
 
       // add timestamps
@@ -320,15 +404,23 @@ var app = new Vue({
       });
     },
 
-    // when someone clicks 'back', restore the home screen
+    /**
+     * Called when the Back button is pressed. Returns to the
+     * home screen with a lit of shopping lists.
+     */
     onBack: function() {
       this.mode='showlist';
       this.pagetitle='Shopping Lists';
     },
 
-    // the use wants to edit an individual shopping list
-    // (not the items, but the meta data)
-    onClickEdit: function(id, title, ev) {
+    /**
+     * Called when the Edit button is pressed next to a shopping list.
+     * We locate the list document by id and change mode to "addlist",
+     * pre-filling the form with that document's details.
+     * @param {String} id
+     * @param {String} title
+     */
+    onClickEdit: function(id, title) {
       this.singleList = this.findDoc(this.shoppingLists, id).doc;
       this.pagetitle = 'Edit - ' + title;
       this.places = [];
@@ -336,8 +428,12 @@ var app = new Vue({
       this.mode='addlist';
     },
 
-    // user wants to delete a shopping list. We kill the parent document
-    // but the items remain in the database
+    /**
+     * Called when the delete button is pressed next to a shopping list.
+     * The shopping list document is located, removed from PouchDB and
+     * removed from Vue's shoppingLists array.
+     * @param {String} id
+     */
     onClickDelete: function(id) {
       var match = this.findDoc(this.shoppingLists, id);
       db.remove(match.doc).then(() => {
@@ -347,14 +443,24 @@ var app = new Vue({
 
     // the user wants to see the contents of a shopping list
     // we load it and switch views
+    /**
+     * Called when the user wants to edit the contents of a shopping list.
+     * The mode is set to 'itemedit'. Vue's currentListId is set to this list's
+     * id field.
+     * @param {String} id
+     * @param {String} title
+     */
     onClickList: function(id, title) {
       this.currentListId = id;
       this.pagetitle = title;
       this.mode = 'itemedit';
     },
 
-    // when a new shopping list item is added
-    // we create a new document and write it to the db
+    /**
+     * Called when a new shopping list item is added. A new shopping list item
+     * object is created with a unique id. It is written to PouchDB and added
+     * to Vue's shoppingListItems array
+     */
     onAddListItem: function() {
       if (!this.newItemTitle) return;
       var obj = JSON.parse(JSON.stringify(sampleListItem));
@@ -370,12 +476,21 @@ var app = new Vue({
       });
     },
 
-    // when an shopping list item is checked, we just need
-    // to keep the database in step
+    /**
+     * Called when an item is checked or unchecked from a shopping list.
+     * The item is located and written to PouchDB
+     * @param {String} id
+     */
     onCheckListItem: function(id) {
       this.findUpdateDoc(this.shoppingListItems, id);
     },
 
+    /**
+     * Called when the Lookup button is pressed. We make an API call to 
+     * OpenStreetMap passing in the user-supplied name of the place. If
+     * the API returns something, the options are added to Vue's "places"
+     * array and become a pull-down list of options on the front end.
+     */
     onClickLookup: function() {
 
       // make request to the OpenStreetMap API
@@ -402,6 +517,12 @@ var app = new Vue({
 
     // when a place is selected from the list, we find the object in the list
     // and copy the lat/long, licence and name over to our database
+    /**
+     * Called when an item is selected from the places pull-down list. The
+     * place is found in the "places" array and its lat/long, licnece and 
+     * address are moved to the Vue object linked with the front-end form.
+     * @param {String} v
+     */
     onChangePlace: function(v) {
       var doc = this.findDoc(this.places, v, 'place_id').doc;
       this.singleList.place.lat = doc.lat;
@@ -410,14 +531,17 @@ var app = new Vue({
       this.singleList.place.address = doc.address;
      },
 
-     // when an item is to be deleted (not just checked), we find the object
-     // delete it in the database and delete it from the UI
+    /**
+     * Called when an item is deleted from a shopping list. We locate the item
+     * in the list, delete it from PouchDB and remove it from the shoppingListItems
+     * Vue array.
+     * @param {String} id
+     */
      onDeleteItem: function(id) {
        var match = this.findDoc(this.shoppingListItems, id);
        db.remove(match.doc).then((data) => {
          this.shoppingListItems.splice(match.i, 1);
        });
      }
-
   }
 })
